d9c61f4c9ea8482756c21574ec55b96a7d91d073
LiYanLance <leeyanly@163.com>
2018-03-29 13:50:30 +0800

    BUILD SUCCESSFUL (section3 PracticeD passed)

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,28 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> collection3 = new HashMap<>();
+        collectionA.forEach(s -> {
+            String  key = s;
+            int value = 1;
+            if(s.contains("-")){
+                value = Integer.parseInt(s.substring(s.indexOf("-") + 1));
+                key = s.substring(0, s.indexOf("-"));
+            }
+            value += collection3.containsKey(key) ? collection3.get(key) : 0;
+            collection3.put(key, value);
+        });
+        collection3.forEach((k,v) ->{
+            if(object.get("value").contains(k)){
+                collection3.put(k, v - v/3);
+            }
+        });
+        return collection3;
     }
 }
