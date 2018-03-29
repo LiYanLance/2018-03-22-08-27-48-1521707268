a07d1c2a6858366189752e8db143ca1659e5c229
LiYanLance <leeyanly@163.com>
2018-03-29 11:12:24 +0800

    section2 PracticeCTest passed

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,32 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> result = new HashMap<>();
+        collection1.forEach(str -> {
+            String key = "", strValue = "";
+            for (int i = 0; i < str.length(); i++) {
+                char tempChar = str.charAt(i);
+                if(tempChar >= 'a' && tempChar <= 'z'){
+                    key += tempChar;
+                } else if (tempChar >= '0' && tempChar <= '9') {
+                    strValue += tempChar;
+                }
+            }
+            int value = strValue.equals("") ? 1 : Integer.parseInt(strValue);
+            if (!result.containsKey(key)) {
+                result.put(key, value);
+            } else {
+                result.put(key, result.get(key) + value);
+            }
+        });
+        return result;
     }
 }
